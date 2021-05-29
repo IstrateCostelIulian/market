33c77abe629b7aa42c1a46f598277474ba36f853
Marius-Daniel <marius.daniel.calarasu@gmail.com>
2021-05-29 23:01:48 +0300

    - update get all method for  OrdersServicesImpl

--- src/main/java/org/internship/market/services/impl/OrdersServicesImpl.java
+++ src/main/java/org/internship/market/services/impl/OrdersServicesImpl.java
@@ -8,13 +8,17 @@ import org.internship.market.database.entity.AccountingEntity;
 import org.internship.market.database.entity.CustomerEntity;
 import org.internship.market.database.entity.OrdersEntity;
 import org.internship.market.database.entity.ProductEntity;
+import org.internship.market.dto.CustomerDTO;
 import org.internship.market.dto.OrdersDTO;
+import org.internship.market.dto.ProductDTO;
 import org.internship.market.services.OrdersServices;
+import org.internship.market.services.mapper.CustomerMapper;
 import org.internship.market.services.mapper.OrderMapper;
+import org.internship.market.services.mapper.ProductMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
+import java.util.LinkedList;
 import java.util.List;
 
 @Service
@@ -25,30 +29,36 @@ public class OrdersServicesImpl implements OrdersServices {
     private final ProductDAO productDAO;
     private final OrderMapper orderMapper;
     private final AccountingDAO accountingDAO;
-    private  final CustomerDAO customerDAO;
+    private final CustomerDAO customerDAO;
+    private final CustomerMapper customerMapper;
+    private final ProductMapper productMapper;
 
     @Autowired
     public OrdersServicesImpl(OrdersDAO ordersDAO, ProductDAO productDAO, OrderMapper orderMapper,
-                              AccountingDAO accountingDAO, CustomerDAO customerDAO) {
+                              AccountingDAO accountingDAO, CustomerDAO customerDAO, CustomerMapper customerMapper,
+                              ProductMapper productMapper) {
         this.ordersDAO = ordersDAO;
         this.productDAO = productDAO;
         this.orderMapper = orderMapper;
         this.accountingDAO = accountingDAO;
         this.customerDAO = customerDAO;
+        this.customerMapper = customerMapper;
+        this.productMapper = productMapper;
     }
 
 
     @Override
-    @Transactional
+
     public void insertOrder(OrdersDTO ordersDTO) {
 
         OrdersEntity ordersEntity = new OrdersEntity();
         ordersEntity.setDate(ordersDTO.getDate());
         ordersEntity.setQuantity(ordersDTO.getQuantity());
 
-        setCustomerOnOrder(ordersDTO,ordersEntity);
+        setCustomerOnOrder(ordersDTO, ordersEntity);
 
         ProductEntity productEntity = productDAO.findProductByName(ordersDTO.getProductName());
+        ordersEntity.setProduct(productEntity);
         ordersEntity.setPrice(orderPrice(ordersDTO, productEntity));
         ordersDAO.createOrder(ordersEntity);
 
@@ -56,20 +66,18 @@ public class OrdersServicesImpl implements OrdersServices {
         productDAO.updateStock(newStock, productEntity.getName());
 
         updateIncomeByOrder(ordersEntity);
-
-
     }
 
-    private void updateIncomeByOrder(OrdersEntity ordersEntity){
+    private void updateIncomeByOrder(OrdersEntity ordersEntity) {
         AccountingEntity accountingEntity = accountingDAO.getAll().get(0);
         double newIncome = accountingEntity.getIncome() + ordersEntity.getPrice();
         accountingDAO.updateIncome(newIncome);
         accountingDAO.updateEconomicBalanceByIncome(accountingEntity.getEconomicBalance());
     }
 
-    private void setCustomerOnOrder(OrdersDTO ordersDTO, OrdersEntity ordersEntity){
-        CustomerEntity customerFound = customerDAO.findByEmail(ordersDTO.getCustomer().getEmailAddress()) ;
-        if(customerFound == null){
+    private void setCustomerOnOrder(OrdersDTO ordersDTO, OrdersEntity ordersEntity) {
+        CustomerEntity customerFound = customerDAO.findByEmail(ordersDTO.getCustomer().getEmailAddress());
+        if (customerFound == null) {
             CustomerEntity customerEntity = new CustomerEntity();
             customerEntity.setName(ordersDTO.getCustomer().getName());
             customerEntity.setSurname(ordersDTO.getCustomer().getSurname());
@@ -77,7 +85,7 @@ public class OrdersServicesImpl implements OrdersServices {
             customerEntity.setEmailAddress(ordersDTO.getCustomer().getEmailAddress());
             customerEntity.setPhoneNumber(ordersDTO.getCustomer().getPhoneNumber());
             ordersEntity.setCustomer(customerEntity);
-        }else {
+        } else {
             ordersEntity.setCustomer(customerFound);
         }
     }
@@ -90,15 +98,29 @@ public class OrdersServicesImpl implements OrdersServices {
     @Override
     public OrdersDTO findOrderByNumber(long orderNUmber) {
         OrdersEntity entity = ordersDAO.findOrderByNumber(orderNUmber);
-        OrdersDTO ordersDTO =  orderMapper.orderEntityToOrderDTO(entity);
+        OrdersDTO ordersDTO = orderMapper.orderEntityToOrderDTO(entity);
         ordersDTO.setProductName(entity.getProduct().getName());
         return ordersDTO;
     }
 
+    // in progress
     @Override
     public List<OrdersDTO> getAll() {
         List<OrdersEntity> ordersEntities = ordersDAO.getAll();
-        return orderMapper.ordersToOderDtoS(ordersEntities);
+        List<OrdersDTO> ordersDTOS = new LinkedList<>();
+        for (OrdersEntity ordersEntity : ordersEntities) {
+            OrdersDTO ordersDTO = new OrdersDTO();
+            CustomerDTO customerDTO = customerMapper.entityToDto(ordersEntity.getCustomer());
+            ProductDTO productDTO = productMapper.entityToDTO(ordersEntity.getProduct());
+            ordersDTO.setProductName(productDTO.getName());
+            ordersDTO.setCustomer(customerDTO);
+            ordersDTO.setDate(ordersEntity.getDate());
+            ordersDTO.setPrice(ordersEntity.getPrice());
+            ordersDTO.setQuantity(ordersEntity.getQuantity());
+            ordersDTO.setOrderNumber(ordersEntity.getOrderNumber());
+            ordersDTOS.add(ordersDTO);
+        }
+        return ordersDTOS;
     }
 
     @Override
